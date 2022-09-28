0e2840b4a634fa349703a24b5a18e5e2b68781fc
skmonjurul <monjurulhaque2011@gmail.com>
2022-09-28 17:36:45 +0530

    added topic bit manipulation 2

---
+++ Intermediate/Topic-011-Bit_Manipulations-2/Assignment/Q1-Help_From_Sam/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solve(int n) {
+        int count = 0;
+        while(n != 0) {
+            if((n & 1) == 1) count++;
+            n = n >> 1;
+        }
+
+        return count;
+    }
+}
