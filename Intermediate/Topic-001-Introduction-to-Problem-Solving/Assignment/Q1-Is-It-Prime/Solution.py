4177441f34b4748bc38c12cfeee333c746ff055d
Samrat <samratbose6@gmail.com>
2023-03-18 16:49:25 +0530

    updated  python code for prime no

--- Intermediate/Topic-001-Introduction-to-Problem-Solving/Assignment/Q1-Is-It-Prime/Solution.py
+++ Intermediate/Topic-001-Introduction-to-Problem-Solving/Assignment/Q1-Is-It-Prime/Solution.py
@@ -1,2 +1,18 @@
-print("Hello")
-print("SK")
+# Defining the Function
+def prime_no(n):
+    c = 0
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            print(n, "Is not a Prime no")
+            c = c+1
+            break
+
+        i = i+1
+
+    if c == 0:
+        print(n, "Is a prime no")
+
+
+# calling  the Function
+prime_no(n=int(input("enter no:- ")))
